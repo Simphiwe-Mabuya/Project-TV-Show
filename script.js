70c5809f3bac797f6428d4c9692338cdabc19d6c
Simpra11 <mrmabuya11@gmail.com>
2025-01-10 23:38:15 +0200

    Provided a link leading back to the specific episode

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function setup() {
     <p><strong>-S0${episode.season}E0${episode.number}</strong></P>
     <img src="${episode.image.medium}" alt="${episode.name}">
     <div class="summary">${episode.summary}</div>
+    <p>Link to the <a href="http://api.tvmaze.com/episodes/4952">Episode: ${episode._links.self.href}</a></p>
     </div>
     `;
 
