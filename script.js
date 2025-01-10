440db49209e57a3df59787b585691fb34d58e3cb
Simpra11 <mrmabuya11@gmail.com>
2025-01-10 02:46:07 +0200

    Started by displaying the episode names

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  let episodes = document.getElementById("root");
+  episodes.innerHTML = '';
+  
+  allEpisodes.forEach((episode) => {
+    const episodeHTML = `
+    <div class="episode">
+    <h3>${episode.name}</h3>
+    </div>
+    `;
+
+    episodes.innerHTML += episodeHTML;
+  });
   makePageForEpisodes(allEpisodes);
+
 }
+
+
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.textContent ;
 }
 
 window.onload = setup;
