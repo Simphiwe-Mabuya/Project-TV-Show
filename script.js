edceed80815f75e4ae68d087e9294c9b17dfd506
Emmanuel <esiziba71@gmail.com>
2025-01-15 12:54:23 +0200

    search functionality

--- script.js
+++ script.js
@@ -29,4 +29,23 @@ function makePageForEpisodes(episodeList) {
  rootElem.textContent ;
 }
 
+const input = document.document.querySelector("#q");
+input.addEventListener("Keyup", function () {
+  const searchTerm = input.value.tolowercase(); //getting the search term and ensuring that it is case sensisitive 
+  const allEpisodes = getAllEpisodes(); //fetching all episodes
+  const filteredEpisodes = allEpisodes.filter(episode => { //filtering the episode array and ensuring that they match the one typed
+    const formattedString = `0${episode.season}E0${episode.number} - ${episode.name}`.toLocaleLowerCase(); //formated the strings for each epidoes to match the "S01E01 - Winter is Coming" requirement. am really not sure how this works but i saw it in a youtube video i was working with to this tasks. i am still trying to understand it
+    return formattedString.includes(searchTerm); //checking if the searched passwords match teh
+  });
+  displayEpisodes(filteredEpisodes); //Showing filltered episodes 
+});
+
+ 
+
+ 
+   
+ 
+
+  
+
 window.onload = setup;
